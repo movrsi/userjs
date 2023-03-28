9c3d51625a4943a24aaab1f2bc6d585715e7067f
moversi <129067651+movrsi@users.noreply.github.com>
2023-03-28 15:55:19 -0400

    Improve SSL hardening thanks to pyllyukko's user.js

--- user.js
+++ user.js
@@ -42,6 +42,26 @@ user_pref("toolkit.telemetry.testing.overrideProductsCheck", false);
 user_pref("toolkit.telemetry.unified", false);
 user_pref("toolkit.telemetry.updatePing.enabled", false);
 
+// PREF: Disable form autofill, don't save information entered in web page forms and the Search Bar
+user_pref("browser.formfill.enable", false);
+
+// PREF: Require manual intervention to autofill known username/passwords sign-in forms
+// http://kb.mozillazine.org/Signon.autofillForms
+// https://www.torproject.org/projects/torbrowser/design/#identifier-linkability
+user_pref("signon.autofillForms", false);
+
+// PREF: Disable formless login capture
+// https://bugzilla.mozilla.org/show_bug.cgi?id=1166947
+user_pref("signon.formlessCapture.enabled", false);
+
+// PREF: When username/password autofill is enabled, still disable it on non-HTTPS sites
+// https://hg.mozilla.org/integration/mozilla-inbound/rev/f0d146fe7317
+user_pref("signon.autofillForms.http", false);
+
+// PREF: Show in-content login form warning UI for insecure login fields
+// https://hg.mozilla.org/integration/mozilla-inbound/rev/f0d146fe7317
+user_pref("security.insecure_field_warning.contextual.enabled", true);
+
 /* Disable experiments */
 user_pref("experiments.supported",	false);
 user_pref("experiments.enabled", false);
@@ -208,7 +228,7 @@ user_pref("browser.startup.homepage_override.buildID", "20100101");
 
 /* Privacy */
 // Reject all Third-Party cookies
-//user_pref("network.cookie.cookieBehavior", 1);
+user_pref("network.cookie.cookieBehavior", 1);
 
 // Disable Referer header
 user_pref("network.http.sendRefererHeader", 0);
@@ -252,13 +272,142 @@ user_pref("javascript.options.ion", false);
 
 
 /* SSL/TLS Hardening */
+// PREF: Enable HSTS preload list (pre-set HSTS sites list provided by Mozilla)
+// https://blog.mozilla.org/security/2012/11/01/preloading-hsts/
+// https://wiki.mozilla.org/Privacy/Features/HSTS_Preload_List
+// https://en.wikipedia.org/wiki/HTTP_Strict_Transport_Security
+user_pref("network.stricttransportsecurity.preloadlist",	true);
+
+// PREF: Enable OCSP Stapling support
+// https://en.wikipedia.org/wiki/OCSP_stapling
+// https://blog.mozilla.org/security/2013/07/29/ocsp-stapling-in-firefox/
+// https://www.digitalocean.com/community/tutorials/how-to-configure-ocsp-stapling-on-apache-and-nginx
+user_pref("security.ssl.enable_ocsp_stapling",			true);
+
+// PREF: Enable OCSP Must-Staple support (Firefox >= 45)
+// https://blog.mozilla.org/security/2015/11/23/improving-revocation-ocsp-must-staple-and-short-lived-certificates/
+// https://www.entrust.com/ocsp-must-staple/
+// https://github.com/schomery/privacy-settings/issues/40
+// NOTICE: Firefox falls back on plain OCSP when must-staple is not configured on the host certificate
+user_pref("security.ssl.enable_ocsp_must_staple",		true);
+
+// PREF: Disable TLS Session Tickets
+// https://www.blackhat.com/us-13/briefings.html#NextGen
+// https://media.blackhat.com/us-13/US-13-Daigniere-TLS-Secrets-Slides.pdf
+// https://media.blackhat.com/us-13/US-13-Daigniere-TLS-Secrets-WP.pdf
+// https://bugzilla.mozilla.org/show_bug.cgi?id=917049
+// https://bugzilla.mozilla.org/show_bug.cgi?id=967977
+user_pref("security.ssl.disable_session_identifiers",		true);
+
+// PREF: Disable insecure TLS version fallback
+// https://bugzilla.mozilla.org/show_bug.cgi?id=1084025
+// https://github.com/pyllyukko/user.js/pull/206#issuecomment-280229645
+user_pref("security.tls.version.fallback-limit", 4);
+
+// PREF: Enforce Public Key Pinning
+// https://en.wikipedia.org/wiki/HTTP_Public_Key_Pinning
+// https://wiki.mozilla.org/SecurityEngineering/Public_Key_Pinning
+// "2. Strict. Pinning is always enforced."
+user_pref("security.cert_pinning.enforcement_level", 2);
+
+// PREF: Disallow SHA-1
+// https://bugzilla.mozilla.org/show_bug.cgi?id=1302140
+// https://shattered.io/
+user_pref("security.pki.sha1_enforcement_level", 1);
+
+// PREF: Warn the user when server doesn't support RFC 5746 ("safe" renegotiation)
+// https://wiki.mozilla.org/Security:Renegotiation#security.ssl.treat_unsafe_negotiation_as_broken
+// https://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2009-3555
+user_pref("security.ssl.treat_unsafe_negotiation_as_broken", true);
+
+
+// PREF: Disable automatic reporting of TLS connection errors
+// https://support.mozilla.org/en-US/kb/certificate-pinning-reports
+// we could also disable security.ssl.errorReporting.enabled, but I think it's
+// good to leave the option to report potentially malicious sites if the user
+// chooses to do so.
+// you can test this at https://pinningtest.appspot.com/
+user_pref("security.ssl.errorReporting.automatic", false);
+
+// PREF: Pre-populate the current URL but do not pre-fetch the certificate in the "Add Security Exception" dialog
+// http://kb.mozillazine.org/Browser.ssl_override_behavior
+// https://github.com/pyllyukko/user.js/issues/210
+user_pref("browser.ssl_override_behavior", 1);
+
+// PREF: Encrypted SNI (when TRR is enabled)
+// https://www.cloudflare.com/ssl/encrypted-sni/
+// https://wiki.mozilla.org/Trusted_Recursive_Resolver#ESNI
+// https://en.wikipedia.org/wiki/Server_Name_Indication#Security_implications_(ESNI)
+user_pref("network.security.esni.enabled", true);
+
 // Disable TLS 1.0 and TLS 1.1
 user_pref("security.tls.version.min", 3);
+user_pref("security.tls.version.max", 4);
 
 // Enable TLS Delegated Credentials
 user_pref("security.tls.enable_delegated_credentials", true);
 
+// PREF: Disable null ciphers
+user_pref("security.ssl3.rsa_null_sha",				false);
+user_pref("security.ssl3.rsa_null_md5",				false);
+user_pref("security.ssl3.ecdhe_rsa_null_sha",			false);
+user_pref("security.ssl3.ecdhe_ecdsa_null_sha",			false);
+user_pref("security.ssl3.ecdh_rsa_null_sha",			false);
+user_pref("security.ssl3.ecdh_ecdsa_null_sha",			false);
+
+// PREF: Disable SEED cipher
+// https://en.wikipedia.org/wiki/SEED
+user_pref("security.ssl3.rsa_seed_sha",				false);
+
+// PREF: Disable 40/56/128-bit ciphers
+// 40-bit ciphers
+user_pref("security.ssl3.rsa_rc4_40_md5",			false);
+user_pref("security.ssl3.rsa_rc2_40_md5",			false);
+// 56-bit ciphers
+user_pref("security.ssl3.rsa_1024_rc4_56_sha",			false);
+// 128-bit ciphers
+user_pref("security.ssl3.rsa_camellia_128_sha",			false);
+user_pref("security.ssl3.ecdhe_rsa_aes_128_sha",		false);
+user_pref("security.ssl3.ecdhe_ecdsa_aes_128_sha",		false);
+user_pref("security.ssl3.ecdh_rsa_aes_128_sha",			false);
+user_pref("security.ssl3.ecdh_ecdsa_aes_128_sha",		false);
+user_pref("security.ssl3.dhe_rsa_camellia_128_sha",		false);
+user_pref("security.ssl3.dhe_rsa_aes_128_sha",			false);
+
+// PREF: Enable GCM ciphers (TLSv1.2 only)
+// https://en.wikipedia.org/wiki/Galois/Counter_Mode
+user_pref("security.ssl3.ecdhe_ecdsa_aes_128_gcm_sha256", true); // 0xc02b
+user_pref("security.ssl3.ecdhe_rsa_aes_128_gcm_sha256", true); // 0xc02f
+
+// PREF: Enable ChaCha20 and Poly1305 (Firefox >= 47)
+// https://www.mozilla.org/en-US/firefox/47.0/releasenotes/
+// https://tools.ietf.org/html/rfc7905
+// https://bugzilla.mozilla.org/show_bug.cgi?id=917571
+// https://bugzilla.mozilla.org/show_bug.cgi?id=1247860
+// https://cr.yp.to/chacha.html
+user_pref("security.ssl3.ecdhe_ecdsa_chacha20_poly1305_sha256", true);
+user_pref("security.ssl3.ecdhe_rsa_chacha20_poly1305_sha256", true);
+
+
 // Disable weak ciphersuites
+user_pref("security.ssl3.ecdh_ecdsa_rc4_128_sha", false);
+user_pref("security.ssl3.ecdh_rsa_rc4_128_sha", false);
+user_pref("security.ssl3.ecdhe_ecdsa_rc4_128_sha", false);
+user_pref("security.ssl3.ecdhe_rsa_rc4_128_sha", false);
+user_pref("security.ssl3.rsa_rc4_128_md5", false);
+user_pref("security.ssl3.rsa_rc4_128_sha", false);
+user_pref("security.tls.unrestricted_rc4_fallback", false);
+
+user_pref("security.ssl3.dhe_dss_des_ede3_sha", false);
+user_pref("security.ssl3.dhe_rsa_des_ede3_sha", false);
+user_pref("security.ssl3.ecdh_ecdsa_des_ede3_sha", false);
+user_pref("security.ssl3.ecdh_rsa_des_ede3_sha", false);
+user_pref("security.ssl3.ecdhe_ecdsa_des_ede3_sha", false);
+user_pref("security.ssl3.ecdhe_rsa_des_ede3_sha", false);
+user_pref("security.ssl3.rsa_des_ede3_sha", false);
+user_pref("security.ssl3.rsa_fips_des_ede3_sha", false);
+
+user_pref("security.ssl3.dhe_rsa_camellia_256_sha", false);
 user_pref("security.ssl3.ecdhe_ecdsa_aes_128_sha", false);
 user_pref("security.ssl3.ecdhe_ecdsa_aes_256_sha", false);
 user_pref("security.ssl3.dhe_rsa_aes_128_sha", false);
@@ -270,6 +419,13 @@ user_pref("security.ssl3.rsa_aes_128_sha", false);
 user_pref("security.ssl3.rsa_aes_256_gcm_sha384", false);
 user_pref("security.ssl3.rsa_aes_256_sha", false);
 user_pref("security.ssl3.rsa_des_ede3_sha", false);
+
+
+// PREF: Disable ciphers with DSA (max 1024 bits)
+user_pref("security.ssl3.dhe_dss_aes_128_sha", false);
+user_pref("security.ssl3.dhe_dss_aes_256_sha", false);
+user_pref("security.ssl3.dhe_dss_camellia_128_sha", false);
+user_pref("security.ssl3.dhe_dss_camellia_256_sha", false);
 
 // Enable Encrypted Client Hello (ECH/ESNI)
 user_pref("network.dns.echconfig.enabled", true);
